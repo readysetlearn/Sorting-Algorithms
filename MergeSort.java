da8abb69db296add4690a5399137c71765c9e4b4
readysetlearn <mister_josh@ymail.com>
2016-05-22 02:42:12 -0700

    Created main class, TestSort.java, for testing sorting implementations. Fixed bug in merge(). Assertions were always failing because the isSort() in ArrayGenerator needed to be rewritten to handle checking a specific range in an array. The mergesort has been tested and confirmed working but the code is messy due to finding the bug/troubleshooting.

--- MergeSort.java
+++ MergeSort.java
@@ -4,50 +4,60 @@ package sortingalgorithms;
 
 public class MergeSort {
     
-    public static void merge(int[] arr, int low, int mid, int high) {
-        // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
+    private static void merge(int[] arr, int low, int mid, int high) {
+        // precondition: a[low .. mid] and a[mid+1 .. high] are sorted subarrays
         assert ArrayGenerator.isSorted(arr, low, mid);
         assert ArrayGenerator.isSorted(arr, mid+1, high);
 
         /*copy contents of array*/
-        for(int k = low; k <= high; k++) {
-            auxiliary[k] = arr[k];
+        for (int k = low; k <= high; k++) {
+            auxiliary[k] = arr[k]; 
         }
+
 
         int i = low, j = mid + 1;//i points to left part of left half, j points to left point of right half        
-        for(int k = low; k < high; k++) {
-            if(i > mid) {
-                arr[k] = auxiliary[j++];//i pointer is exhausted
-            } else if(j > high) {
-                arr[k] = auxiliary[i++];//j pointer is exhausted
-            } else if(auxiliary[j] < auxiliary[i]) {
-                arr[k] = auxiliary[j++];
-            } else {
-                arr[k] = auxiliary[i++];
-            }
+        for (int k = low; k <= high; k++) {
+            if      (i > mid)              arr[k] = auxiliary[j++];//i pointer is exhausted
+            else if (j > high)               arr[k] = auxiliary[i++];
+            else if ((auxiliary[j] < auxiliary[i])) arr[k] = auxiliary[j++];//j pointer is exhausted
+            else                           arr[k] = auxiliary[i++];
         }
-        
-        
-        // postcondition: a[lo .. hi] is sorted
+
+        // postcondition: a[low .. high] is sorted
         assert ArrayGenerator.isSorted(arr, low, high);
-    }
+ }
 
     /*Top-down implementation of mergesort*/
-    public static void sort(int[] a) {
-            sort(a, 0, a.length-1);
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(int[] arr) {
+        auxiliary = new int[arr.length];
+        sort(arr, 0, arr.length-1);
+        assert isSorted(arr);
     }
     
     /*Top-down implementation of mergesort*/
-    private static void sort(int[] a, int lo, int hi) {
-	if (hi <= lo) {
-            return;
-	}
-        int mid = lo + (hi - lo) / 2;
-	sort(a, lo, mid);
-	sort(a, mid+1, hi);
-	merge(a, lo, mid, hi);
+    // mergesort a[low..high] using auxiliary array aux[lo..hi]
+    private static void sort(int[] arr, int low, int high) {
+        if (high <= low) return;
+        int mid = low + (high - low) / 2;
+        sort(arr, low, mid);
+        sort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
     }
 
+    
+    private static boolean isSorted(int[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(int[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if ((a[i] < a[i-1])) return false;
+        return true;
+    }
 
     
     private static int[] auxiliary;    
